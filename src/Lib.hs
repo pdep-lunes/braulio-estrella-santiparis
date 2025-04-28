745832b69626fda3caad8b21cdbae292a5b5740f
Santiago Paris Puebla <parispueblasantiago@gmail.com>
2025-04-28 20:54:17 -0300

    Mejorando las funciones que retornan personajes actualizados

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Lib () where
 
 import Text.Show.Functions ()
@@ -15,19 +14,19 @@ restarVida vidaContrincante vidaARestar
   | otherwise = vidaContrincante - vidaARestar
 
 hacerDanio :: Personaje -> Int -> Personaje
-hacerDanio contrincante danio = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (restarVida (vida contrincante) danio)
+hacerDanio contrincante danio = contrincante {vida = restarVida (vida contrincante) danio}
 
 curarVida :: Personaje -> Int -> Personaje
-curarVida aliado aCurar = UnPersonaje (nombre aliado) (poderBasico aliado) (superPoder aliado) (superPoderActivo aliado) (vida aliado + aCurar)
+curarVida aliado aCurar = aliado {vida = vida aliado + aCurar}
 
 cambiarNombre :: Personaje -> String -> Personaje
-cambiarNombre personaje aAgregar = UnPersonaje (nombre personaje ++ aAgregar) (poderBasico personaje) (superPoder personaje) (superPoderActivo personaje) (vida personaje)
+cambiarNombre personaje aAgregar = personaje {nombre = nombre personaje ++ aAgregar}
 
 desactivarSuperPoder :: Personaje -> Personaje
-desactivarSuperPoder personaje = UnPersonaje (nombre personaje) (poderBasico personaje) (superPoder personaje) False (vida personaje)
+desactivarSuperPoder personaje = personaje {superPoderActivo = False}
 
 activarSuperPoder :: Personaje -> Personaje
-activarSuperPoder personaje = UnPersonaje (nombre personaje) (poderBasico personaje) (superPoder personaje) True (vida personaje)
+activarSuperPoder personaje = personaje {superPoderActivo = True}
 
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa contrincante = hacerDanio contrincante 1000
