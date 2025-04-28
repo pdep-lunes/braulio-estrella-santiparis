f354d0852177e48f3b138df1eee7b9d2cf2de6ba
Santiago Paris Puebla <parispueblasantiago@gmail.com>
2025-04-28 11:33:51 -0300

    Funcion granadaDeEspinas creada

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@ module Lib () where
 
 import Text.Show.Functions ()
 
-data Personaje = UnPersonaje {nombre :: String, poderBasico :: String, superPoder :: String, superPoderActivo :: Bool, vida :: Int}
+data Personaje = UnPersonaje {nombre :: String, poderBasico :: String, superPoder :: String, superPoderActivo :: Bool, vida :: Float}
 
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa contrincante = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (vida contrincante - 1000)
@@ -12,3 +12,9 @@ lluviaDeTuercas tipoAtaque contraparte
   | tipoAtaque == "sanadoras" = UnPersonaje (nombre contraparte) (poderBasico contraparte) (superPoder contraparte) (superPoderActivo contraparte) (vida contraparte + 800)
   | tipoAtaque == "daninas" = UnPersonaje (nombre contraparte) (poderBasico contraparte) (superPoder contraparte) (superPoderActivo contraparte) (vida contraparte / 2)
   | otherwise = contraparte
+
+granadaDeEspinas :: Int -> Personaje -> Personaje
+granadaDeEspinas radio contrincante 
+  | radio > 3 && vida contrincante < 800 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) False 0
+  | radio > 3 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (vida contrincante - 1000)
+  | otherwise = bolaEspinosa contrincante
