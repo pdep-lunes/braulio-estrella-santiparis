24c29144b0f1a437d563d5af823f4ebf8feb65ee
Santiago Paris Puebla <parispueblasantiago@gmail.com>
2025-04-28 11:46:56 -0300

    Funcion torretaCurativa creada y funcion restarVida creada

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,17 +4,25 @@ import Text.Show.Functions ()
 
 data Personaje = UnPersonaje {nombre :: String, poderBasico :: String, superPoder :: String, superPoderActivo :: Bool, vida :: Float}
 
+restarVida :: Float -> Float -> Float
+restarVida vidaContrincante vidaARestar 
+  | vidaARestar >= vidaContrincante = 0
+  | otherwise = vidaContrincante - vidaARestar 
+
 bolaEspinosa :: Personaje -> Personaje
-bolaEspinosa contrincante = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (vida contrincante - 1000)
+bolaEspinosa contrincante = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (restarVida (vida contrincante) 1000)
 
 lluviaDeTuercas :: String -> Personaje -> Personaje
-lluviaDeTuercas tipoAtaque contraparte 
-  | tipoAtaque == "sanadoras" = UnPersonaje (nombre contraparte) (poderBasico contraparte) (superPoder contraparte) (superPoderActivo contraparte) (vida contraparte + 800)
-  | tipoAtaque == "daninas" = UnPersonaje (nombre contraparte) (poderBasico contraparte) (superPoder contraparte) (superPoderActivo contraparte) (vida contraparte / 2)
-  | otherwise = contraparte
+lluviaDeTuercas tipoAtaque otroPersonaje 
+  | tipoAtaque == "sanadoras" = UnPersonaje (nombre otroPersonaje) (poderBasico otroPersonaje) (superPoder otroPersonaje) (superPoderActivo otroPersonaje) (vida otroPersonaje + 800)
+  | tipoAtaque == "daninas" = UnPersonaje (nombre otroPersonaje) (poderBasico otroPersonaje) (superPoder otroPersonaje) (superPoderActivo otroPersonaje) (vida otroPersonaje / 2)
+  | otherwise = otroPersonaje
 
 granadaDeEspinas :: Int -> Personaje -> Personaje
 granadaDeEspinas radio contrincante 
   | radio > 3 && vida contrincante < 800 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) False 0
   | radio > 3 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (vida contrincante)
   | otherwise = bolaEspinosa contrincante
+
+torretaCurativa :: Personaje -> Personaje
+torretaCurativa aliado = UnPersonaje (nombre aliado) (poderBasico aliado) (superPoder aliado) True (vida aliado * 2)
