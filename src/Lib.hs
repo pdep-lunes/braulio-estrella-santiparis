b49dadfd9d82cc8da1c8e17889790b0858c10f8a
Santiago Paris Puebla <parispueblasantiago@gmail.com>
2025-04-28 15:46:50 -0300

    Creadas las funciones hacerDanio, curarVida, cambiarNombre, desactivarSuperPoder y activarSuperPoder

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,30 +1,46 @@
 module Lib () where
 
 import Text.Show.Functions ()
+import Foreign (alignPtr)
 
-data Personaje = UnPersonaje {nombre :: String, poderBasico :: String, superPoder :: String, superPoderActivo :: Bool, vida :: Int}
+data Personaje = UnPersonaje {nombre :: String, poderBasico :: Personaje -> Personaje, superPoder :: Personaje -> Personaje, superPoderActivo :: Bool, vida :: Int} deriving (Show)
 
 type Partida = [Personaje]
 
 restarVida :: Int -> Int -> Int
-restarVida vidaContrincante vidaARestar 
+restarVida vidaContrincante vidaARestar
   | vidaARestar >= vidaContrincante = 0
-  | otherwise = vidaContrincante - vidaARestar 
+  | otherwise = vidaContrincante - vidaARestar
+
+hacerDanio :: Personaje -> Int -> Personaje
+hacerDanio contrincante danio = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (restarVida (vida contrincante) danio)
+
+curarVida :: Personaje -> Int -> Personaje
+curarVida aliado aCurar = UnPersonaje (nombre aliado) (poderBasico aliado) (superPoder aliado) (superPoderActivo aliado) (vida aliado + aCurar)
+
+cambiarNombre :: Personaje -> String -> Personaje
+cambiarNombre personaje aAgregar = UnPersonaje (nombre personaje ++ aAgregar) (poderBasico personaje) (superPoder personaje) (superPoderActivo personaje) (vida personaje)
+
+desactivarSuperPoder :: Personaje -> Personaje
+desactivarSuperPoder personaje = UnPersonaje (nombre personaje) (poderBasico personaje) (superPoder personaje) False (vida personaje)
+
+activarSuperPoder :: Personaje -> Personaje
+activarSuperPoder personaje = UnPersonaje (nombre personaje) (poderBasico personaje) (superPoder personaje) True (vida personaje)
 
 bolaEspinosa :: Personaje -> Personaje
-bolaEspinosa contrincante = UnPersonaje (nombre contrincante) (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (restarVida (vida contrincante) 1000)
+bolaEspinosa contrincante = hacerDanio contrincante 1000
 
 lluviaDeTuercas :: String -> Personaje -> Personaje
-lluviaDeTuercas tipoAtaque otroPersonaje 
-  | tipoAtaque == "sanadoras" = UnPersonaje (nombre otroPersonaje) (poderBasico otroPersonaje) (superPoder otroPersonaje) (superPoderActivo otroPersonaje) (vida otroPersonaje + 800)
-  | tipoAtaque == "daninas" = UnPersonaje (nombre otroPersonaje) (poderBasico otroPersonaje) (superPoder otroPersonaje) (superPoderActivo otroPersonaje) (div (vida otroPersonaje) 2)
+lluviaDeTuercas tipoAtaque otroPersonaje
+  | tipoAtaque == "sanadoras" = curarVida otroPersonaje 800
+  | tipoAtaque == "daninas" = hacerDanio otroPersonaje (div (vida otroPersonaje) 2)
   | otherwise = otroPersonaje
 
 granadaDeEspinas :: Int -> Personaje -> Personaje
-granadaDeEspinas radio contrincante 
-  | radio > 3 && vida contrincante < 800 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) False 0
-  | radio > 3 = UnPersonaje (nombre contrincante ++ " Espina estuvo aqui") (poderBasico contrincante) (superPoder contrincante) (superPoderActivo contrincante) (vida contrincante)
+granadaDeEspinas radio contrincante
+  | radio > 3 && vida contrincante < 800 = cambiarNombre (hacerDanio (desactivarSuperPoder contrincante) (vida contrincante)) " Espina estuvo aqui"
+  | radio > 3 = cambiarNombre contrincante " Espina estuvo aqui"
   | otherwise = bolaEspinosa contrincante
 
 torretaCurativa :: Personaje -> Personaje
-torretaCurativa aliado = UnPersonaje (nombre aliado) (poderBasico aliado) (superPoder aliado) True (vida aliado * 2)
+torretaCurativa aliado = activarSuperPoder.curarVida aliado $ vida aliado
